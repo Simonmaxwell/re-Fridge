595d032087145e3fc6a527b282a9a416245d2a25
The Good Maxwell <maxwellraden@gmail.com>
2017-02-23 20:52:25 -0600

    laying the foundation

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 'use strict'
 
-const APIKEY = "f164427952ade2ca59a10717ddb7ecc2";
-const APPID =  "1a6c0d8f";
+$(document).ready(function() {
+	const APIKEY = "f164427952ade2ca59a10717ddb7ecc2";
+	const APPID =  "1a6c0d8f";
+	
+	$("#search-form").submit(event => {
+			event.preventDefault();
+			let query = $("#query").val();
+			getRequest(query);
+		});
+	
+		const getRequest = (query, token) => { 
+			let params = {
+				app_id: APPID,
+				app_key: APIKEY,
+				q: query
+				}
+			};
 
-$("#search-form").submit(event => {
-		event.preventDefault();
-		let query = $("#query").val();
-		getRequest(query);
-	});
+		$.getJSON(URL, params, data => {
 
-	const getRequest = (query, token) => { 
-		let params = {
-			app_id: APPID,
-			app_key: APIKEY,
-			q: query
 		};
+});
