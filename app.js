91cdc8e0bc772c5fdd422236292b4388ca4cd818
The Good Maxwell <maxwellraden@gmail.com>
2017-02-23 20:38:47 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const APIKEY = "f164427952ade2ca59a10717ddb7ecc2";
+const APPID =  "1a6c0d8f";
+
+$("#search-form").submit(event => {
+		event.preventDefault();
+		let query = $("#query").val();
+		getRequest(query);
+	});
+
+	const getRequest = (query, token) => { 
+		let params = {
+			app_id: APPID,
+			app_key: APIKEY,
+			q: query
+		};
