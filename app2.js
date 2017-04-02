d50ab33fbd341c30d7dbfc9deaafc2bebd902916
The Good Maxwell <maxwellraden@gmail.com>
2017-04-02 11:05:17 -0500

    big crazy mess

--- app2.js
+++ app2.js
@@ -4,12 +4,16 @@ $(document).ready(function() {
 	const APPID =  "1a6c0d8f";
 	const URL = "https://api.edamam.com/search";
 	let ingredients = [];
+	let nongredients = [];
 	let fridgeManifest = [];
+	let fridgeNonifest = [];
 
 	const getRequest = (query) => { 
 		let params = {
 			app_id: APPID,
 			app_key: APIKEY,
+			from: 0,
+			to: 50,
 			q: query
 		};
 		
@@ -33,36 +37,67 @@ $(document).ready(function() {
 				listOfIngredients: ingredientListItems
 			});
 		}
-	generateRecipeHtml(recipes);
+	generateRecipeHtml(recipeFilter(recipes));
 	}
+
+	const colorForIngredient = (ingredients, ingredient) => {
+		let color = 'red';
+		for ( let i = 0; i < ingredients.length; i++) {
+			if( ingredient.indexOf(ingredients[i]) != -1) {
+				color = 'black';
+			};
+		}
+		return color;
+	};
 
 	const generateIngredientsHtml = (listOfIngredients) => {
 		let ingredientsHtml = '';
-		let color = 'red';
+		let color = '';
 		for ( let i = 0; i < listOfIngredients.length; i++){
 			for ( let k = 0; k < listOfIngredients[i].length; k++){
-				if( listOfIngredients[i].indexOf(ingredients[i]) != -1) {
-				color = 'black';
-				};
+				color = colorForIngredient(ingredients, listOfIngredients[i]);	
 			};
-		ingredientsHtml += `<li class="${color}">${individual ingredient line}</li>`;
+			ingredientsHtml += `<li class="${color}">${listOfIngredients[i]}</li>`;
 		}
+		return ingredientsHtml;
 	}
 
 	const generateRecipeHtml = (recipes) => {
 		var html = '';
-		let ingredientsHtml = 'butts';
+		let ingredientsHtml = '';
 		for (let i = 0; i < recipes.length; i++) {
 			html += `<div class="recipe recipe-box">
 					<a target ="_blank" href="${recipes[i].recipeUrl}">
-					<img src="${recipes[i].recipeImageUrl}">
+					<img class="image" src="${recipes[i].recipeImageUrl}">
 					<h2>${recipes[i].recipeName}</h5> 
 					</a>
-					<ul >${generateIngredientsHtml(recipes[i].listOfIngredients)}</ul>
+					<button type="toggle"> Show Ingredients </button>
+					<div class="ingredient-list">
+						<ul>${generateIngredientsHtml(recipes[i].listOfIngredients)}</ul>
+					</div>
 					</div>`
 		};
 		$("#results").html(html);
 		ingredients = [];
+		nongredients = [];
+	};
+
+	const recipeFilter = (recipes) => {
+		let goodRecipes = [];
+		let recipeValidity = true;
+		for (let i = 0; i < recipes.length; i++) {
+			for (let k = 0; k < recipes[i].listOfIngredients.length; k++) {
+				for ( let j = 0; j < nongredients.length; j++) {
+					if (recipes[i].listOfIngredients[k].indexOf(nongredients[j]) != -1) {
+						recipeValidity = false;
+					};
+				};
+			};
+			if (recipeValidity) {
+				goodRecipes.push(recipes[i]);
+			}
+		};
+		return goodRecipes;
 	};
 
 	$("#add-item").submit(event => {
@@ -74,13 +109,25 @@ $(document).ready(function() {
 		$("#ingredient").val("");
 	});
 
+	$("#exclude-item").submit(event => {
+		event.preventDefault();
+		var poodItem = $("#nongredient").val();
+		fridgeNonifest += `<li>${poodItem}</li>`;
+		nongredients.push(poodItem);
+		$("#fridge-nonifest").html(fridgeNonifest);
+		$("#nongredient").val("");			
+	});
+
 	$("#search-form").submit(event => {
 		event.preventDefault();
 		let query = ingredients.join(", ");
-		getRequest(query);	
+		getRequest(query);
+		$("#fridge-manifest").html("");
+		$("#fridge-nonifest").html("");
+
 	});
 
-
-
-
+	$("#results").click(event => {
+		$(".ingredient-list").toggle();
+	});
 });
